9cd5e8c27f4a3933f649be9024a9731d31b538ab
Comi <comi.hu@comihu-MBP.local>
2017-07-06 17:11:16 +0800

    實作上下頁, 實作在oops頁面可返回上一頁, 整理程式碼, fix minor bugs

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var helmet = require('helmet');
+var request = require('request');
+
+var schema = require('./public/js/schema.js');
+
 var app = express();
 
-var bodyParser = require('body-parser');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));    // to support URL-encoded bodies
 
-
-var mongoose = require('mongoose');
+//for access mongodb
 mongoose.connect('mongodb://mongo-msgdb:27017/msgdb');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () { console.log("Database Connected."); });
 
-//PostSchema
-var schema = require('./public/js/schema.js');
+//db PostSchema
 var post = mongoose.model('post', schema);
 
-
 app.set('view engine', 'ejs');
-
 
 //set where static file
 app.use(express.static(__dirname + '/public'));
 
-
-var cookieParser = require('cookie-parser');
+//for cookie use
 app.use(cookieParser());
 
 var router = express.Router();
 app.use('/', router);
 
-var helmet = require('helmet');
+//security?
 app.use(helmet());
-app.use(helmet.frameguard());
+app.disable('x-powered-by');
 
-//request
-var request = require("request");
-
+// find ip request options
 var options = { 
     method: 'GET',
     url: 'https://tools.clifflu.net/ip',
@@ -48,9 +48,8 @@ var options = {
     json: true
 };
 
-
+//how many posts in a list page
 const page_posts = 5;
-
 
 
 router.get('/post/:postId', function (req, res) {
@@ -75,28 +74,21 @@ router.all('/list/:page', function (req, res) {
 
     if(req.method === 'GET') {
 
-        if(!req.params.page.match(/^[\d]+$/g)){
+        //page only can be a positive number no lead zero
+        if(!req.params.page.match(/^[1-9][0-9]*$/g)){
             res.status(400);
             res.render('oops', {msg: 'Invalid url !'});
         }
 
         let skipNum = page_posts * (+req.params.page - 1);
 
-        // Not complete!
-        // make page number > posts number ,redirect. but it can return to top function
-        //
-        // post.count({unlink: false}, function(err, count) {
-        //     if(skipNum > count)
-        //         return res.redirect('/list/1');
-        // });
-
         return post.find({unlink: false}).sort('-postId').skip(skipNum).limit(page_posts).exec(function(err, result) {
             if (err)
                 console.log('find post error ' + err);
 
-            if(result) {
+            if(result && result.length > 0) {
                 res.status(200);
-                return res.render('list', {result, username : req.cookies.username || 'vistor'});
+                return res.render('list', {result, username : req.cookies.username || 'visitor'});
             }
 
             res.status(404);
@@ -106,7 +98,7 @@ router.all('/list/:page', function (req, res) {
     }
     if(req.method === 'POST') {
 
-        let username = req.cookies.username;
+        let username = req.cookies.username || 'visitor';
 
         if(req.body.hasOwnProperty('remove')  && username !== 'visitor') {
 
@@ -122,8 +114,6 @@ router.all('/list/:page', function (req, res) {
                 res.redirect('/list/1');
 
             });
-            
-
         }
         if(req.body.hasOwnProperty('edit')  && username !== 'visitor') {
             console.log('edit post')
@@ -134,34 +124,39 @@ router.all('/list/:page', function (req, res) {
                 else
                     res.redirect('/list/1');
             });
-
         }
         if(req.body.hasOwnProperty('read')) {
             console.log('read post')
             return res.redirect('/post/' + req.body.read);
-
-
         }
-        
-
+        if(req.body.hasOwnProperty('new')) {
+            return res.redirect('/new');
+        }
+        if(req.body.hasOwnProperty('back')) {
+            return res.redirect('/');
+        }
+        if(req.body.hasOwnProperty('pre')) {
+            let page = +req.params.page-1;
+            return res.redirect('/list/' + page);
+        }
+        if(req.body.hasOwnProperty('next')) {
+            let page = +req.params.page+1;
+            return res.redirect('/list/' + page);
+        }
     }
-    return res.redirect('/list/1');
-
+    res.status(401);
+    return res.render('oops', {msg: 'You are visitor or not an author of the post'});
 });
-
-
-
 
 router.all('/edit/:postId', function (req, res) {
 
     if(req.method === 'GET') {
-        
 
-        post.findOne({postId: req.params.postId} ,function(err, result){
+        return post.findOne({postId: req.params.postId} ,function(err, result){
             if (err)
                 console.log('find post error ' + err);
 
-            let username = req.cookies.username;
+            let username = req.cookies.username || 'visitor';
 
             if(result && result.author !== username || username === 'visitor') {
                 res.status(401);
@@ -174,11 +169,9 @@ router.all('/edit/:postId', function (req, res) {
 
             res.status(404);
             res.render('oops', {msg: 'Post not find or been remove Q_Q'});
-
-
         });
     }
-    else if(req.method === 'POST' && req.body.hasOwnProperty('post')) {
+    if(req.method === 'POST' && req.body.hasOwnProperty('post')) {
 
         let editData = {
 
@@ -186,39 +179,31 @@ router.all('/edit/:postId', function (req, res) {
             message:   req.body.message,
 
         };
-
         post.findOneAndUpdate({postId: req.params.postId}, editData, {upsert:true}, function(err, result){
             if (err) return res.send(500, { error: err });
 
             console.log("success update");
         });
 
-        res.redirect('/post/' + req.params.postId); 
-    }else {
-
-        res.redirect('/list/1')
-        
+        return res.redirect('/post/' + req.params.postId); 
     }
-
+    res.status(500);
+    res.render('oops', {msg: 'server error'});
 });
-
-
 
 router.all('/new', function (req, res) {
 
     if(req.method === 'GET') {
         console.log(req.method + ' 127.0.0.1:3000/new')
         res.status(200);
-        res.render('newpost', {content: 'Write Something', username: req.cookies.username})
+        return res.render('newpost', {content: 'Write Something', username: req.cookies.username || 'visitor'})
     }
-    else if(req.method === 'POST' && req.body.hasOwnProperty('post')) {
+    if(req.method === 'POST' && req.body.hasOwnProperty('post')) {
 
         console.log(req.method + ' 127.0.0.1:3000/new');
 
-
         //count post in collections(posts)
-        post.count({}, function(err, count) {
-
+        return post.count({}, function(err, count) {
 
             var postId = count + 1;
 
@@ -243,28 +228,21 @@ router.all('/new', function (req, res) {
 
         });
 
-    }else {
-        res.redirect('/list/1');
     }
-
-
+    res.status(500);
+    res.render('oops', {msg: 'server error'});
 });
-
-
 
 router.all(['/','/index'], function (req, res) {
     if(req.method === 'GET') {
         console.log(req.method + ' 127.0.0.1:3000/')
-        
-
 
         if (req.cookies.username) {
             console.log(req.cookies.username + ' repeat visit!');
-
-        } else {
-            res.cookie('username', 'visitor', {maxAge: 600 * 1000});
-            console.log('first visit')
+            return res.render('oops', {msg: 'Invalid username. You can only use [A-Za-z0-9]'});
         }
+        res.cookie('username', 'visitor', {maxAge: 600 * 1000});
+        console.log('first visit');
 
         post.count({unlink: false}, function(err, nowPosts){
 
@@ -280,14 +258,11 @@ router.all(['/','/index'], function (req, res) {
                     res.render('index', {username: req.cookies.username || 'visitor', nowPosts, totalPosts, Ip});
                 });
 
-
-
             });
 
-        });
-   
+        });  
     }
-    else if(req.method === 'POST'  && req.body.hasOwnProperty('login')) {
+    if(req.method === 'POST'  && req.body.hasOwnProperty('login')) {
         console.log('login')
         if(req.body.username.match(/^[a-zA-Z0-9\s]+$/g)){
             console.log(req.body.username + ' good username')
@@ -300,17 +275,15 @@ router.all(['/','/index'], function (req, res) {
         }
 
     }
-    else if(req.method === 'POST'  && req.body.hasOwnProperty('visit')) {
+    if(req.method === 'POST'  && req.body.hasOwnProperty('visit')) {
 
         console.log('login by visitor')
         res.cookie('username', 'visitor' , {maxAge: 600 * 1000});
         res.redirect('/list/1');
 
     }
-    else {
-        console.log('redirect /')
-        res.redirect('/');
-    }
+    console.log('redirect /')
+    res.redirect('/');
 
 });
 
